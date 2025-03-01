97d7bde394ed794b0f419c85bf83ad5ab5eacc84
iambin6 <miraisan2512@gmail.com>
2025-03-01 22:22:13 +0700

    Add files via upload

--- Week5/Course.java
+++ Week5/Course.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Week5;
 
-/**
- *
- * @author ADMIN
- */
+import java.util.List;
+import java.util.Scanner;
+
 public class Course {
-    
+
+    private String courseID;
+    private String courseName;
+    private int credits;
+
+    public Course() {
+        this.courseID = "";
+        this.courseName = "";
+        this.credits = 0;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public void inputAll(Scanner sc, List<Course> course) {
+        courseID = Utils.checkId("CousreID: ", course);
+        courseName = Utils.checkName("Course name: ", course);
+        credits = Utils.checkCredit("Credits: ");
+
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" + "courseID=" + courseID + ", courseName=" + courseName + ", credits=" + credits + '}';
+    }
+
 }
--- Week5/CourseManagement.java
+++ Week5/CourseManagement.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Week5;
 
-/**
- *
- * @author ADMIN
- */
-public class CourseManagement {
-    
+public class OnlineCourse extends Course {
+    private String platform;
+    private String instructor;
+    private String note;
+
+    public OnlineCourse() {
+        super();
+        this.platform = "";
+        this.instructor = "";
+        this.note = "";
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public void inputAll(String id, String name, int credits, String platform, String instructor, String note) {
+        setCourseID(id);
+        setCourseName(name);
+        setCredits(credits);
+        this.platform = platform;
+        this.instructor = instructor;
+        this.note = note;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "OnlineCourse{" + "platform=" + platform + ", instructor=" + instructor + ", note=" + note + '}';
+    }
 }
